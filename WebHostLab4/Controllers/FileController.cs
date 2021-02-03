591d79336c9ab1d0b3c76a11ef3f43a4480c3d54
kiril0id <42606503+kiril0id@users.noreply.github.com>
2021-02-03 15:22:27 +0300

    Добавьте файлы проекта.

---
+++ WebHostLab4/Controllers/FileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebHostLab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : Controller
+    {
+        private readonly IWebHostEnvironment _appEnvironment;
+        public FileController(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        [HttpGet("{filename}")]
+        public IActionResult GetFile(string filename)
+        {
+            string file_name = filename + ".txt";
+            string file_path = Path.Combine(_appEnvironment.ContentRootPath, "File/" + file_name);
+            string file_type = "application/pdf";
+
+
+            return PhysicalFile(file_path, file_type, file_name);
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string file_path = Path.Combine(_appEnvironment.ContentRootPath, "File/");
+              string[] files = Directory.GetFiles(file_path);
+
+            return files;
+        }   
+    }
+}
